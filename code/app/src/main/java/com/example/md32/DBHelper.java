5a70ecfb13d85abd6cc7c25879c3edc2266d9785
Dmitriy Hoperskiy <dh@gusi-lebedi.ru>
2022-06-08 00:52:57 +0300

    md 3.2 added

---
+++ code/app/src/main/java/com/example/md32/DBHelper.java
@@ -0,0 +1,25 @@
+package com.example.md32;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+    public DBHelper(Context context) {
+        super(context, "MyDB", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table mytable ("
+                + "id integer primary key autoincrement,"
+                + "name text" + ");");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
+
